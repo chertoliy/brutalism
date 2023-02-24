3e33f1735e0be43e440d39b9adfdffa77bb1d2e9
chertoliy <104504367+chertoliy@users.noreply.github.com>
2023-02-24 14:39:03 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+const body = document.querySelector('body')
 const slide0 = document.querySelector('.slide-00');
 const slide1 = document.querySelector('.slide-01');
 const slide2 = document.querySelector('.slide-02');
 const slide3 = document.querySelector('.slide-03');
 const slide4 = document.querySelector('.slide-04');
+const resetButton = document.querySelector('.background__link');
 
 slide0.addEventListener('wheel', checkScrollDirection);
 function checkScrollDirection(event) {
@@ -17,7 +19,7 @@ function checkScrollDirectionIsDown(event) {
    return event.deltaY > 0;
 };
 
-slide1.onscroll = function () {scrollEnd1()}; 
+slide1.onscroll = function () { scrollEnd1() };
 function scrollEnd1() {
    if ((slide1.scrollTop + slide1.clientHeight) == slide1.scrollHeight) {
       slide1.classList.add('slide--close');
@@ -26,7 +28,7 @@ function scrollEnd1() {
    };
 };
 
-slide2.onscroll = function () {scrollEnd2()}; 
+slide2.onscroll = function () { scrollEnd2() };
 function scrollEnd2() {
    if ((slide2.scrollTop + slide2.clientHeight) == slide2.scrollHeight) {
       slide2.classList.add('slide--close');
@@ -35,7 +37,7 @@ function scrollEnd2() {
    };
 };
 
-slide3.onscroll = function () {scrollEnd3()}; 
+slide3.onscroll = function () { scrollEnd3() };
 function scrollEnd3() {
    if ((slide3.scrollTop + slide3.clientHeight) == slide3.scrollHeight) {
       slide3.classList.add('slide--close');
@@ -44,11 +46,48 @@ function scrollEnd3() {
    };
 };
 
-slide4.onscroll = function () {scrollEnd4()}; 
+slide4.onscroll = function () { scrollEnd4() };
 function scrollEnd4() {
    if ((slide4.scrollTop + slide4.clientHeight) == slide4.scrollHeight) {
       slide4.classList.add('slide--close');
    } else {
       slide4.classList.remove('slide--close');
    };
+};
+
+resetButton.onclick = function () { resetSlide0(), resetSlide1(), resetSlide2(), resetSlide3(), resetSlide4() };
+function resetSlide0() {
+   slide0.classList.remove('slide--close');
+};
+
+function resetSlide1() {
+   slide1.classList.remove('slide--close');
+};
+function resetSlide2() {
+   slide2.classList.remove('slide--close');
+};
+function resetSlide3() {
+   slide3.classList.remove('slide--close');
+};
+function resetSlide4() {
+   slide4.classList.remove('slide--close');
+};
+
+window.onload = function () { mobileDisableScrollEnd() };
+function mobileDisableScrollEnd() {
+if (window.innerWidth < 376) {
+   checkScrollDirection = function () { };
+   checkScrollDirectionIsDown = function () { };
+   scrollEnd1 = function () { };
+   scrollEnd2 = function () { };
+   scrollEnd3 = function () { };
+   scrollEnd4 = function () { };
+   };
+   //    checkScrollDirection = checkScrollDirection;
+   //    checkScrollDirectionIsDown = checkScrollDirectionIsDown;
+   //    scrollEnd1 = scrollEnd1;
+   //    scrollEnd2 = scrollEnd2;
+   //    scrollEnd3 = scrollEnd3;
+   //    scrollEnd4 = scrollEnd4;
+   //    console.log('No');
 };
