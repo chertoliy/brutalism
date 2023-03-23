55ceefdd53cef7f3b13f6ee5dee6603d452d4286
Anatolii Chervontsev <104504367+chertoliy@users.noreply.github.com>
2023-03-23 09:28:34 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const slide4 = document.querySelector('.slide-04');
 const resetButton = document.querySelector('.background__reset');
 
 // Remove slides after end of scroll
-slide0.addEventListener('wheel', checkScrollDirection);
+slide0.onwheel = checkScrollDirection;
 function checkScrollDirection(event) {
    if (checkScrollDirectionIsDown(event)) {
       slide0.classList.add('slide--close');
@@ -21,40 +21,35 @@ function checkScrollDirectionIsDown(event) {
    return event.deltaY > 0;
 };
 
-slide1.onscroll = scrollEnd1;
-function scrollEnd1() {
+slide1.onscroll = () => {
    if ((slide1.scrollTop + slide1.clientHeight) == slide1.scrollHeight) {
       slide1.classList.add('slide--close');
       slide2.classList.remove('scroll--disabled');
    };
 };
 
-slide2.onscroll = scrollEnd2;
-function scrollEnd2() {
+slide2.onscroll = () => {
    if ((slide2.scrollTop + slide2.clientHeight) == slide2.scrollHeight) {
       slide2.classList.add('slide--close');
       slide3.classList.remove('scroll--disabled');
    };
 };
 
-slide3.onscroll = scrollEnd3;
-function scrollEnd3() {
+slide3.onscroll = () => {
    if ((slide3.scrollTop + slide3.clientHeight) == slide3.scrollHeight) {
       slide3.classList.add('slide--close');
       slide4.classList.remove('scroll--disabled');
    };
 };
 
-slide4.onscroll = scrollEnd4;
-function scrollEnd4() {
+slide4.onscroll = () => {
    if ((slide4.scrollTop + slide4.clientHeight) == slide4.scrollHeight) {
       slide4.classList.add('slide--close');
    };
 };
 
 // Reset slides to initial states
-resetButton.onclick = resetSlides;
-function resetSlides() {
+resetButton.onclick = () => {
    setTimeout(() => slide0.classList.remove('slide--close'), 1800);
    setTimeout(() => {
       slide1.classList.remove('slide--close');
@@ -79,8 +74,7 @@ function resetSlides() {
 };
 
 // Mobile version listener to disable desktop functions
-window.onload = mobileDisableScrollEnd;
-function mobileDisableScrollEnd() {
+window.onload = () => {
    if (window.innerWidth <= 415) {
       checkScrollDirectionIsDown = () => { };
    };
